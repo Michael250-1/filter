6474e1aab7504feabdc1869a23b3ff26abd3975d
Michael250-1 <79066678+Michael250-1@users.noreply.github.com>
2021-10-08 18:59:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+clownnose=loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
 }
 function draw(){
 image(video,0,0,300,300);
+image(clownnose,noseX,noseY,30,30);
 }
 function setup(){
 canvas=createCanvas(300,300);
@@ -23,8 +26,10 @@ function modelLoaded(){
 function gotposes(results){
     if(results.length>0){
         console.log(results);
-        console.log("nosex= " + results[0].pose.nose.x);
-        console.log("nosey= " + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y-10;
+        console.log("nosex= " + noseX);
+        console.log("nosey= " + noseY);
     }
     
 }
